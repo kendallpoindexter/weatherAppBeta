7dbe0bae9f8acb4df49c3744b57aac5d9b382b07
kendall <kendall.poindexter@detroitlabs.com>
2020-05-25 20:32:49 -0400

    create location service file

---
+++ weatherAppBeta/weatherAppBeta/LocationService .swift
@@ -0,0 +1,9 @@
+//
+//  LocationService .swift
+//  weatherAppBeta
+//
+//  Created by Kendall Poindexter on 5/25/20.
+//  Copyright © 2020 Kendall Poindexter. All rights reserved.
+//
+
+import Foundation
