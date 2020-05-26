93dfe1d3040659a84d496476e8c1eb3abd1aa864
kendall <kendall.poindexter@detroitlabs.com>
2020-05-25 20:44:07 -0400

    Create WeatherData Model

---
+++ weatherAppBeta/weatherAppBeta/Model/WeatherModel.swift
@@ -0,0 +1,32 @@
+//
+//  WeatherModel.swift
+//  weatherAppBeta
+//
+//  Created by Kendall Poindexter on 5/25/20.
+//  Copyright © 2020 Kendall Poindexter. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let weather: [Weather]
+    let main: Main
+}
+
+struct Weather: Decodable {
+    let description: String
+    let icon: String
+}
+
+struct Main: Decodable {
+    let temp: Double
+    let lowTemp: Double
+    let highTemp: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case lowTemp = "temp_min"
+        case highTemp = "temp_max"
+    }
+}
